1ff5c9c0f4902c75322f76e8ff807d1e9f043259
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-04-21 09:43:58 +0100

    maybe

--- deploy.sh
+++ deploy.sh
@@ -1,52 +1,119 @@
 #!/bin/sh
 set -e
-cd "`dirname $0`"
+
+echo "------ ${WHAT} deploy to ${DRONE_DEPLOY_TO} with ${IMAGE_NAME} on ${BRANCH} ------"
+
+BRANCH_SAFE=$(echo ${BRANCH} | sed -e "s/\//_/g")
 
 export KUBE_SERVER=https://kube-dev.dsp.notprod.homeoffice.gov.uk
-export ENVIRON=dev
-export KUBE_NAMESPACE=ircbd-${ENVIRON}
+export KUBE_NAMESPACE=ircbd-${DRONE_DEPLOY_TO}
+
 export INSECURE_SKIP_TLS_VERIFY=true
 
 export KEYCLOAK_DISCOVERY=https://keycloak.digital.homeoffice.gov.uk/auth/realms/ircbd
 export KEYCLOAK_CLIENT_ID=ircbd-dev
 
-export API_URL=api-ircbd.notprod.homeoffice.gov.uk
-export WALLBOARD_URL=wallboard-ircbd.notprod.homeoffice.gov.uk
+RANDOM_STRING=$(head /dev/urandom | tr -dc a-z0-9 | head -c 13)
 
-NEED_MYSQL="eph dev int"
 
-NEED_REDIS="eph dev int uat prod"
+### --- DEFAULT SETTINGS --- ###
+export API_URL=api-ircbd-${DRONE_DEPLOY_TO}.notprod.homeoffice.gov.uk
+export WALLBOARD_URL=wallboard-ircbd-${DRONE_DEPLOY_TO}.notprod.homeoffice.gov.uk
 
-if [ ${DEPlOY_MYSQL} ]; then
+
+
+#export KEYCLOAK_CLIENT_SECRET=${KEYCLOAK_CLIENT_SECRET_${DRONE_DEPLOY_TO}}
+
+export DBNAME=removals
+export DBUSER=root
+export DBPASS=foobar
+export DBHOST=mysql
+export DBPORT=3306
+
+### --- PRODUCTION SETTINGS --- ###
+if [ $DRONE_DEPLOY_TO = "prod" ]; then
+  export KUBE_TOKEN=${KUBE_TOKEN_PROD}
+  export KUBE_SERVER=https://kube.dsp.digital.homeoffice.gov.uk
+  export API_URL=api.ircbd.homeoffice.gov.uk
+  export WALLBOARD_URL=wallboard.ircbd.homeoffice.gov.uk
+fi
+
+
+### --- EPHEMERAL SETTINGS --- ###
+if [ $DRONE_DEPLOY_TO = "ephemeral" ]; then
+  export KUBE_NAMESPACE=ircbd-${RANDOM_STRING}
+  export KUBE_SERVER=${KUBE_SERVER_CI}
+  export KUBE_TOKEN=${KUBE_TOKEN_CI}
+
+  kubectl create namespace ${KUBE_NAMESPACE} --insecure-skip-tls-verify=true --server=${KUBE_SERVER} --token=${KUBE_TOKEN}
+
+  DEPLOY_API=TRUE
+  DEPLOY_WALLBOARD=TRUE
+  RUN_TESTS=TRUE
+
+  export API_URL=api-ircbd-${RANDOM_STRING}.notprod.homeoffice.gov.uk
+  export WALLBOARD_URL=wallboard-ircbd-${RANDOM_STRING}.notprod.homeoffice.gov.uk
+
+  kd -f kube/e2etest/deployment.yml -f kube/e2etest/service.yml
+fi
+
+### we use RDS in UAT/PROD so don't need a mysql server there ###
+if [ $DRONE_DEPLOY_TO != "prod" -a $DRONE_DEPLOY_TO != "uat" ]; then
   kd -f kube/mysql/deployment.yml -f kube/mysql/service.yml
 fi
 
-if [ ${DEPLOY_REDIS} ]; then
-  kd -f kube/redis/deployment.yml -f kube/redis/service.yml
+
+if [ $WHAT = api ]; then
+  DEPLOY_API=TRUE
+  export API_IMAGE=${IMAGE_NAME}
 fi
 
-if [ ${DEPLOY_SELENIUM} ]; then
-  kd -f kube/e2etest/deployment.yml -f kube/e2etest/service.yml
+if [ $WHAT = wallboard ]; then
+  DEPLOY_WALLBOARD=TRUE
+  export WALLBOARD_IMAGE=${IMAGE_NAME}
 fi
 
 
 if [ ${DEPLOY_API} ]; then
+  if [ -z ${API_IMAGE} ]; then
+    if [ $(wget -sq https://quay.io/c1/squash/ukhomeofficedigital/removals-integration/${BRANCH_SAFE}) ]; then
+      export API_IMAGE=quay.io/ukhomeofficedigital/removals-integration:${BRANCH_SAFE}
+    else
+      export API_IMAGE=quay.io/ukhomeofficedigital/removals-integration:origin_master
+    fi
+  fi
+  echo "Deploying API ${API_IMAGE} to ${API_URL}"
   kd \
+    -f kube/redis/deployment.yml \
+    -f kube/redis/service.yml \
     -f kube/api/task.yml \
     -f kube/api/deployment.yml \
     -f kube/api/service.yml \
     -f kube/api/ingress.yml
 fi
 
-if ${DEPLOY_WALLBOARD}; then
+if [ ${DEPLOY_WALLBOARD}  ]; then
+  if [ -z ${WALLBOARD_IMAGE} ]; then
+    if [ $(wget -sq https://quay.io/c1/squash/ukhomeofficedigital/removals-wallboard/${BRANCH_SAFE}) ]; then
+      export WALLBOARD_IMAGE=quay.io/ukhomeofficedigital/removals-wallboard:${BRANCH_SAFE}
+    else
+      export WALLBOARD_IMAGE=quay.io/ukhomeofficedigital/removals-wallboard:origin_master
+
+    fi
+  fi
+  echo "Deploying WALLBOARD ${WALLBOARD_IMAGE} to ${WALLBOARD_URL}"
   kd \
   -f kube/wallboard/deployment.yml \
   -f kube/wallboard/service.yml \
   -f kube/wallboard/ingress.yml
 fi
 
+if [ $DRONE_DEPLOY_TO = "ephemeral" ]; then
+  kubectl delete namespace ${KUBE_NAMESPACE} --insecure-skip-tls-verify=true --server=${KUBE_SERVER} --token=${KUBE_TOKEN}
+fi
 
-if ${RUN_TESTS}; then
+
+if [ ${RUN_TESTS} ]; then
   kd \
   -f kube/e2etest/task.yml
 # @TODO check the test results
